959f55bc1cee5c3a2f55542d48280b7e5ede3ae9
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-04-28 11:08:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
 return   fetch('https://anapioficeandfire.com/api/books')
+<<<<<<< HEAD
   .then((resp) => resp.json())
+=======
+  .then(resp => resp.json());
+>>>>>>> 85f3b58c7a013f13c5ca6b7d5366ea0cdfe600a1
   .then((json)=> {renderBooks(json);}
   );
 }
